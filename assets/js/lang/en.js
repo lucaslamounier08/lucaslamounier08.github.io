c503425581f43f3468ea02e57789ff4a454f1343
Lucas Lamounier <lucaslamounier08@gmail.com>
2021-01-25 22:13:43 -0300

    fully updated and internationalization completed

--- assets/js/lang/en.js
+++ assets/js/lang/en.js
@@ -1,7 +1,90 @@
 const i18nLangs = {
     page_title: "Portfolio - Lucas Lamounier Faria",
-
+    
     name: "Lucas Lamounier Faria",
     portfolio: "Portifolio",
+    profile_title: "Computer Engineer",
+    profile_certification: "PSM 1 & PSPO 1 certified. Java Developer.",
+    profile_contact: "  Contact",
 
+    language: "Language  ",
+    language_portuguese: "Portuguese",
+    language_english: "English",
+
+    section_about: "About",
+    section_about_bullet1: "&bull; From Minas Gerais, Brazil. Graduated in Computer Engineering - Universidade Federal de Itajubá, Brazil.",
+    section_about_bullet2: "&bull; Worked as a developer in a dairy cattle monitoring startup.",
+    section_about_bullet3: "&bull; Freelancer as general translator and subjects about Blockchain, virtual assistant relating to SEO - Search Engine Optimization.",
+    section_about_bullet4: "&bull; Seeking improvement and delivering continuous value.",
+
+    section_experience: "Experience",
+
+    section_experience_tcs_title: "Support Analyst",
+    section_experience_tcs_company: "Tata Consultancy Services",
+    section_experience_tcs_date: "Feb. 2020 - Currently",
+    section_experience_tcs_description: "ITIL support focused on Minor Enhancements, Scrum Master.",
+    section_experience_tcs_description2: "Analysis and development of Minor Enhancements in legacy applications using Oracle Forms and Reports, Oracle PL / SQL and Java. Scrum Master removing impediments.",
+
+    section_experience_b2ml_title: "Java Web Developer",
+    section_experience_b2ml_company: "B2ML Sistemas",
+    section_experience_b2ml_date: "Jun. 2019 - Feb. 2020",
+    section_experience_b2ml_description: "Software Developer for Web based applications.",
+    section_experience_b2ml_description2: "Experience in Java EE, Java Server Faces (JSF), UI Framework (PrimeFaces), Hibernate, Spring, JavaScript, Tomcat, Apache, Git, Scrum, Blender and Blender4Web, Payara Server and Database management.",
+
+    section_experience_kaufberater_title: "Brazilian Representative",
+    section_experience_kaufberater_company: "Kaufberater.io",
+    section_experience_kaufberater_date: "Jan. 2018 - Jan. 2020",
+    section_experience_kaufberater_description: "Freelancer and virtual assistant under contract for hours.",
+    section_experience_kaufberater_description2: "Commercial representative focused on prospecting customers, managing payments and supervising paid publications aiming at SEO engagement and activation.",
+
+    section_experience_startup_title: "Android Developer",
+    section_experience_startup_company: "TechFarm - Startup",
+    section_experience_startup_date: "Mar. 2016 - Nov. 2016",
+    section_experience_startup_description: "Android application developer for a dairy cattle care and maintenance startup.",
+    section_experience_startup_description2: "",
+
+    section_projects: "My Projects - Android & Web",
+
+    section_flappy_name: "Flappy Edit Creator",
+    section_flappy_description: "A version of the late Flappy Bird, but fully customizable! Cut out your friends faces and use them as the bird, the obstacle or even the background image of the game!",    
+    section_flappy_download: " Download - Google Play",
+
+    section_qualrole_name: "Qual Rolê? (Indisponível)",
+    section_qualrole_description: "Qual Rolê? was an application that sought parties near you, using events registered on Facebook for this. After the Facebook scandal with Cambridge Analytica, all forms of access to data have been blocked, and unfortunately by disabling the features of this app.", 
+    section_qualrole_download: " Download - Google Play",
+
+    section_engbeer_name: "EngBeer - Qual cerveja compensa comprar",
+    section_engbeer_description: "You don't need to be an engineer to calculate which beer has the best cost benefit, enter the price of the beers and EngBeer calculates for you! ",
+    section_engbeer_download: " Download - Google Play",
+
+    section_aroundtheworldnews_name: "Around the World News - Youtube",
+    section_aroundtheworldnews_description: "A completely automated YouTube channel, searching for the latest news, converting to audio and adding everything to images.",
+    section_aroundtheworldnews_view: " Acessar",
+
+    section_informations: "Info",
+    section_informations_curriculum: "Curriculum",
+    section_informations_location: "Alphaville, Barueri - São Paulo, Brazil",
+    section_informations_email: "lucaslamounier08@gmail.com",
+    section_informations_upwork: "Upwork",
+
+    section_education: "Education and Certificates",
+    section_education_bachelor_name: "Computer Engineering",
+    section_education_bachelor_description: "Universidade Federal de Itajubá - UNIFEI",
+    section_education_bachelor_years: "(2014-2019)",
+
+    section_education_certificate_psm1: "Professional Scrum Master 1",
+    section_education_certificate_psm1_description: "Professional Scrum Master 1 (PSM 1) - Scrum.org",
+    section_education_certificate_psm1_year: "(2020)",
+
+    section_education_certificate_pspo1: "Professional Scrum Product Owner 1",
+    section_education_certificate_pspo1_description: "Professional Scrum Product Owner 1 (PSPO 1) - Scrum.org",
+    section_education_certificate_pspo1_year: "(2020)",
+
+    section_languages: "Languages",
+    section_languages_portuguese: "Portuguese:",
+    section_languages_english: "English:",
+    section_languages_level_advanced: "Advanced",
+    section_languages_level_native: "Native speaker",
+
+    section_visit_counter: "Visit Counter",
 }
--- assets/js/lang/pt.js
+++ assets/js/lang/pt.js
@@ -3,15 +3,88 @@ const i18nLangs = {
     
     name: "Lucas Lamounier Faria",
     portfolio: "Portifolio",
-    profile_title: "Engenheiro da Computação.",
+    profile_title: "Engenheiro da Computação",
     profile_certification: "PSM 1 & PSPO 1 certificado. Desenvolvedor Java.",
-    profile_contact: "Contato",
+    profile_contact: "  Contato",
+
+    language: "Idioma  ",
+    language_portuguese: "Português",
+    language_english: "Inglês",
 
     section_about: "Sobre",
-    section_about_bullet1: "&bull; Graduado em Engenharia de Computação pela Universidade Federal de Itajubá, Brasil.",
+    section_about_bullet1: "&bull; Mineiro. Graduado em Engenharia de Computação pela Universidade Federal de Itajubá.",
     section_about_bullet2: "&bull; Atuou como desenvolvedor em uma Startup de monitoramento de gado leiteiro.",
     section_about_bullet3: "&bull; Freelancer como tradutor geral e assuntos sobre Blockchain, assistente virtual relacionando SEO - Search Engine Optimization.",
-    section_about_bullet4: "&bull; Atualmente desenvolve aplicativos para Android.",
+    section_about_bullet4: "&bull; Buscando aperfeiçoamento e entrega de valor contínuo.",
 
+    section_experience: "Experiência",
 
+    section_experience_tcs_title: "Analista de Suporte",
+    section_experience_tcs_company: "Tata Consultancy Services",
+    section_experience_tcs_date: "Fev. 2020 - Atualmente",
+    section_experience_tcs_description: "Suporte ITIL focado em Minor Enhancements, Scrum Master.",
+    section_experience_tcs_description2: "Análise e desenvolvimento de Minor Enhancements em aplicações legadas utilizando Oracle Forms e Reports, Oracle PL/SQL e Java. Scrum Master removendo impedimentos.",
+
+    section_experience_b2ml_title: "Desenvolvedor Java Web",
+    section_experience_b2ml_company: "B2ML Sistemas",
+    section_experience_b2ml_date: "Jun. 2019 - Fev. 2020",
+    section_experience_b2ml_description: "Desenvolvedor de Software voltado a Java Web.",
+    section_experience_b2ml_description2: "Experiência em Java EE, Java Server Faces (JSF), UI Framework (PrimeFaces), Hibernate, Spring, JavaScript, Tomcat, Apache, Git, Scrum, Blender e Blender4Web, Payara Server e gerenciamento de Banco de Dados.",
+
+    section_experience_kaufberater_title: "Representante Comercial",
+    section_experience_kaufberater_company: "Kaufberater.io",
+    section_experience_kaufberater_date: "Jan. 2018 - Jan. 2020",
+    section_experience_kaufberater_description: "Freelancer e assistente virtual sob contrato de horas.",
+    section_experience_kaufberater_description2: "Representante comercial focado em prospectar clientes, gerenciar pagamentos e supervisionando publicações pagas almejando engajamento e ativação de SEO.",
+
+    section_experience_startup_title: "Desenvolvedor Android",
+    section_experience_startup_company: "TechFarm - Startup",
+    section_experience_startup_date: "Mar. 2016 - Nov. 2016",
+    section_experience_startup_description: "Desenvolvedor de aplicativo Android para uma startup de cuidados e manutenção de gado leiteiro.",
+    section_experience_startup_description2: "",
+
+    section_projects: "Meus projetos - Android & Web",
+
+    section_flappy_name: "Flappy Edit Creator",
+    section_flappy_description: "Uma versão do finado Flappy Bird, mas totalmente customizável! Recorte o rosto de seus amigos e use como o pássaro, o obstáculo ou até mesmo a imagem de fundo do jogo!",
+    section_flappy_download: " Download - Google Play",
+
+    section_qualrole_name: "Qual Rolê? (Indisponível)",
+    section_qualrole_description: "Qual Rolê? era um aplicativo que buscava festas próximas a você, utilizando para isto eventos cadastrados no Facebook. Após o escândalo do Facebook com a Cambridge Analytica, todas formas de acesso aos dados foram bloqueadas, e infelizmente inativando as funcionalidades deste app.",
+    section_qualrole_download: " Download - Google Play",
+
+    section_engbeer_name: "EngBeer - Qual cerveja compensa comprar",
+    section_engbeer_description: "Você não precisa ser engenheiro para calcular qual cerveja tem melhor custo benefício, entre com o preço das cervejas e EngBeer calcula para você!",
+    section_engbeer_download: " Download - Google Play",
+
+    section_aroundtheworldnews_name: "Around the World News - Youtube",
+    section_aroundtheworldnews_description: "Um canal no YouTube completamente automatizado, buscando as notícias mais recentes, convertendo para áudio e juntando tudo a imagens.",
+    section_aroundtheworldnews_view: " Acessar",
+
+    section_informations: "Informações",
+    section_informations_curriculum: "Currículo",
+    section_informations_location: "Alphaville, Barueri - São Paulo",
+    section_informations_email: "lucaslamounier08@gmail.com",
+    section_informations_upwork: "Upwork",
+
+    section_education: "Educação e Certificados",
+    section_education_bachelor_name: "Engenharia da Computação",
+    section_education_bachelor_description: "Universidade Federal de Itajubá - UNIFEI",
+    section_education_bachelor_years: "(2014-2019)",
+
+    section_education_certificate_psm1: "Professional Scrum Master 1",
+    section_education_certificate_psm1_description: "Professional Scrum Master 1 (PSM 1) pela Scrum.org",
+    section_education_certificate_psm1_year: "(2020)",
+
+    section_education_certificate_pspo1: "Professional Scrum Product Owner 1",
+    section_education_certificate_pspo1_description: "Professional Scrum Product Owner 1 (PSPO 1) pela Scrum.org",
+    section_education_certificate_pspo1_year: "(2020)",
+
+    section_languages: "Idiomas",
+    section_languages_portuguese: "Português:",
+    section_languages_english: "Inglês:",
+    section_languages_level_advanced: "Avançado",
+    section_languages_level_native: "Falante nativo",
+
+    section_visit_counter: "Contador de visitas",
 }
